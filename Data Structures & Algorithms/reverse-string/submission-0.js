0e0a7b6a476b8a59355f2e8dd6e2664d2847934b
darshit-singh <op.dare7@gmail.com>
2026-04-22 10:52:29 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/reverse-string/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s) {
+        let start = 0
+        let end = s.length - 1
+        while (start <= end){
+            this.swap(s, start, end)
+            start++;
+            end--;
+        }
+
+    }
+    swap(s, start, end){
+        let temp = s[start]
+        s[start] = s[end]
+        s[end] = temp
+    }
+}
